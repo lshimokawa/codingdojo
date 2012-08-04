b26f2e6879dcd37455cacd5e8b1723dad0a656dd
Lennon Shimokawa <lshimokawa@gmail.com>
2012-08-04 01:01:09 -0500

    Java JUnit

---
+++ java-junit/src/main/java/codingdojo/Calculator.java
@@ -0,0 +1,9 @@
+package codingdojo;
+
+public class Calculator {
+	
+	public int add(String input) {
+		return 0;
+	}
+	
+}
---
+++ java-junit/src/test/java/codingdojo/CalculatorTest.java
@@ -0,0 +1,15 @@
+package codingdojo;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testShouldReturnZeroWithEmptyString() {
+		Calculator calculator = new Calculator();
+		int result = calculator.add("");
+		assertEquals(0, result);
+	}
+
+}
