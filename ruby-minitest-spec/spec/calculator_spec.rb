e3505527dbdd24cc3a53fb352c39510c04b3593c
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-08-06 12:07:47 -0500

    use a module for collect methods

--- ruby-minitest-spec/spec/calculator_spec.rb
+++ ruby-minitest-spec/spec/calculator_spec.rb
@@ -1,10 +1,8 @@
-require "minitest/autorun"
-require_relative "../lib/calculator"
+require 'minitest/autorun'
+require_relative '../lib/calculator'
 
 describe Calculator do
-  it "should return 0 on empty string" do
-    calculator = Calculator.new
-    result = calculator.add ""
-    result.must_equal 0
+  it 'should return 0 on empty string' do
+    Calculator.add('').must_equal 0
   end
 end
--- ruby-minitest-unit/test/calculator_test.rb
+++ ruby-minitest-unit/test/calculator_test.rb
@@ -1,12 +1,8 @@
-require "minitest/autorun"
-require_relative "../lib/calculator"
+require 'minitest/autorun'
+require_relative '../lib/calculator'
 
 class CalculatorTest < MiniTest::Unit::TestCase
-
   def test_return_0_on_empty_string
-    calculator = Calculator.new
-    result = calculator.add ""
-    assert_equal 0, result
+    assert_equal 0, Calculator.add('')
   end
-
 end
