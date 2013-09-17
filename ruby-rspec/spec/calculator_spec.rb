8f082fdbbbc82333c8397a5de0fe4d544ab02626
Lennon Shimokawa <lshimokawa@gmail.com>
2013-09-17 17:08:46 -0500

    Update rspec syntax

--- ruby-rspec/spec/calculator_spec.rb
+++ ruby-rspec/spec/calculator_spec.rb
@@ -3,6 +3,6 @@ require_relative '../lib/calculator'
 
 describe Calculator do
   it 'should return 0 on empty string' do
-    Calculator.add('').should == 0
+    expect(Calculator.add('')).to eq 0
   end
 end
